deab3205f90e678e658afb0d72ae43f96a68c03a
zlzelu <3117669226@qq.com>
2024-11-27 23:49:04 +0800

    函数学习

---
+++ 函数学习/函数学习/hanshuxuexi.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+//strcmp
+//int main()
+//{
+//	char a[] = "heblo,word";
+//	char b[] = "heblo word";
+//	int c = strcmp(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
+//strchr”Îstrrchr
+//int main()
+//{
+//	char a[] = "hello,word";
+//	char* p = strchr(a, 'o');
+//	printf("%s", p);
+//	printf("%s", strchr(a, 'o'));
+//	return 0;
+//}
